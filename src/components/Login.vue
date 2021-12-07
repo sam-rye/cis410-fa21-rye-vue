c84363abbbe4730f3c1cbfed4b915fe12df0f738
unknown <srye@rams.colostate.edu>
2021-12-07 14:20:00 -0700

    13.2 11/18 axios, signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Login</h1>
+    <div v-if="this.$route.query.signupsuccess" class="alert alert-success">
+      Thanks for signing up, please login now.
+    </div>
   </div>
 </template>
 
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,11 +1,99 @@
 <template>
   <div>
     <h1>Sign Up</h1>
+    <form @submit.prevent="onSubmit">
+      <div class="mb-3">
+        <label for="fname-input" class="form-label">First name</label
+        ><input
+          type="text"
+          class="form-control"
+          id="fname-input"
+          required=""
+          placeholder="First name"
+          v-model="nameFirst"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="lname-input" class="form-label">Last name</label
+        ><input
+          type="text"
+          class="form-control"
+          id="lname-input"
+          required=""
+          placeholder="Last name"
+          v-model="nameLast"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="email-input" class="form-label">Email</label
+        ><input
+          type="email"
+          class="form-control"
+          id="email-input"
+          required=""
+          placeholder="Enter email"
+          v-model="email"
+        />
+        <small v-if="dupEmail" class="text-danger"
+          >Please choose a different email</small
+        >
+      </div>
+      <div class="mb-3">
+        <label for="password-input" class="form-label">Password</label
+        ><input
+          type="password"
+          class="form-control"
+          id="password-input"
+          placeholder="Password"
+          required=""
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      <p id="error-signup" class="text-danger">{{ errorMessage }}</p>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      nameFirst: "",
+      nameLast: "",
+      email: "",
+      password: "",
+      errorMessage: "",
+      dupEmail: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      //console.log("form submitted");
+      const myFormData = {
+        nameFirst: this.nameFirst,
+        nameLast: this.nameLast,
+        email: this.email,
+        password: this.password,
+      };
+      //console.log(myFormData);
+      axios
+        .post("/attendee", myFormData)
+        .then((myResponse) => {
+          console.log("the response", myResponse);
+          this.$router.replace("/login?signupsuccess=true");
+        })
+        .catch((myError) => {
+          if (myError.response.status === 409) {
+            this.dupEmail = true;
+          } else {
+            this.errorMessage = "Cannot sign you up, please try again later";
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
