911b38b794ea5f385fa4f788155b36c38ad50da8
unknown <srye@rams.colostate.edu>
2021-12-07 15:57:24 -0700

    15.1 11/30 Vuex, login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,11 +4,86 @@
     <div v-if="this.$route.query.signupsuccess" class="alert alert-success">
       Thanks for signing up, please login now.
     </div>
+
+    <form @submit.prevent="onSubmit">
+      <div class="mb-3">
+        <label for="email-input" class="form-label">Password</label
+        ><input
+          type="email"
+          class="form-control"
+          id="email-input"
+          required=""
+          placeholder="Enter email"
+          v-model="email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password-input" class="form-label">Password</label
+        ><input
+          type="password"
+          class="form-control"
+          id="password-input"
+          placeholder="Password"
+          required=""
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+
+      <p v-if="credentialsError" class="form-text text-danger">
+        Invalid credentials
+      </p>
+
+      <p v-if="loginError" class="form-text text-danger">
+        Could not log you in, please try again later
+      </p>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      loginError: false,
+      credentialsError: false,
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      let myFormData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      //console.log("the form data", myFormData);
+
+      axios
+        .post("/attendee/login", myFormData)
+        .then((myResponse) => {
+          //console.log("the response", myResponse);
+
+          this.$store.commit("storeTokenInApp", myResponse.data.token);
+          this.$store.commit("storeUserInApp", myResponse.data.user);
+
+          this.$router.replace("/account");
+        })
+        .catch((myError) => {
+          console.log("error in /attendee/login", myError);
+
+          if (myError.response.status == 401) {
+            this.credentialsError = true;
+          } else {
+            this.loginError = true;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    token: null,
+    user: null,
+    movies: [],
+  },
+  mutations: {
+    storeTokenInApp(state, myToken) {
+      state.token = myToken;
+    },
+
+    storeUserInApp(state, theUser) {
+      state.user = theUser;
+    },
+  },
+  actions: {},
+});
